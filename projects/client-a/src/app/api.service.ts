62aa22ed2dd8a21e93406ed4613366443f7e8e24
kazelionheart <link_lan4@hotmail.com>
2020-07-03 19:39:00 -0400

    asdasd

--- projects/client-a/src/app/api.service.ts
+++ projects/client-a/src/app/api.service.ts
@@ -1,15 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class ApiService {
-  constructor(private httpClient: HttpClient) {}
 
-  public getSaints() {
-    return this.httpClient.get(
-      `https://saint-seiya-api.herokuapp.com/api/characters`
-    );
-  }
+  constructor() { }
 }
